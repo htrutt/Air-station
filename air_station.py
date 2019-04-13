f6dfc15ac647902e905570849a4104784f8a93a6
htrutt <herve.trutt@gmail.com>
2019-04-13 18:23:01 +0200

    Save data to InfluxDB instead of a file

--- air_station.py
+++ air_station.py
@@ -1,12 +1,38 @@
 #!/bin/python3
 from particule_sensor import ParticuleSensor
+from influxdb import InfluxDBClient
+
+# TODO signal catcher for shutdown
+# TODO env variable instead for easy config within docker
+DB_NAME = 'luftdata'
 
 
 class AirStation:
     def __init__(self):
-        self._particule_sensor = ParticuleSensor()
 
-    def  start_station(self):
+        # TODO change hostname when moving this code to docker container
+        self.db_client = InfluxDBClient(host='localhost', port=8086)
+        db_list = self.db_client.get_list_database()
+        print(db_list)
+        self.db_exists = False
+        for db in db_list:
+            if db["name"] == DB_NAME:
+                self.db_exists = True
+
+        if not self.db_exists:
+            print("Database does not exist, will create it")
+            self.db_client.create_database(dbname=DB_NAME)
+            self.db_client.create_retention_policy(name="Retention_policy",
+                                                   duration="52w",
+                                                   replication="1",
+                                                   database=DB_NAME,
+                                                   shard_duration="1w")
+
+        self.db_client.switch_database(DB_NAME)
+
+        self._particule_sensor = ParticuleSensor(self.db_client)
+
+    def start_station(self):
         self._particule_sensor.read_port()
 
 
--- particule_sensor.py
+++ particule_sensor.py
@@ -3,6 +3,7 @@ import datetime
 
 import serial
 from serial.tools.list_ports import grep
+from influxdb import InfluxDBClient
 
 VID = "1a86"
 PID = "7523"
@@ -15,7 +16,7 @@ class InvalidDataException(Exception):
 
 class ParticuleSensor:
 
-    def __init__(self):
+    def __init__(self, db_client):
         try:
             ports = list(serial.tools.list_ports.comports())
 
@@ -24,13 +25,12 @@ class ParticuleSensor:
                     self._port_path = p.device
 
             self._port = serial.Serial(self._port_path)
-            self._current_day = None
+            self.db_client = db_client  # type: InfluxDBClient
 
         except serial.SerialException as e:
             print(e)
             raise
 
-    # TODO break out the writing to file to be able to save in same file temp + PM data
     def read_port(self):
         while True:
             rcv = self._port.read(10)
@@ -38,20 +38,21 @@ class ParticuleSensor:
             print(rcv)
 
             try:
-                PM25, PM10 = self._parse_data(rcv)
-                self._save_to_file(PM25, PM10)
+                PM2_5, PM10 = self._parse_data(rcv)
+                self._save_to_db(PM2_5, PM10)
             except InvalidDataException as e:
                 print("Invalid data received")
 
-    def _save_to_file(self, PM25, PM10):
-        now = datetime.datetime.now()
-        if now.day != self._current_day:
-            self._current_day = now.day
-            self._file_name = "/tmp/data_{}_{}_{}".format(now.year, now.month, now.day)
+    def _save_to_db(self, PM2_5, PM10):
+        json_ojbect =[{
+                "measurement": "ParticuleData",
+                "fields": {
+                    "PM2_5": PM2_5,
+                    "PM10": PM10
+                }
+            }]
 
-        with open(self._file_name, mode='a')as data_file:
-            data_file_writer = csv.writer(data_file, delimiter=',')
-            data_file_writer.writerow([datetime.datetime.now(), PM25, PM10])
+        self.db_client.write_points(points=json_ojbect, time_precision="s")
 
     def _parse_data(self, data):
         if bytes([data[0]]) == b'\xAA' and bytes([data[1]]) == b'\xC0' and bytes([data[9]]) == b'\xAB':
@@ -61,9 +62,9 @@ class ParticuleSensor:
                 calculated_check += data[i]
 
             if (calculated_check % 256) == data[8]:
-                PM25 = (data[3] * 256 + data[2]) / 10
+                PM2_5 = (data[3] * 256 + data[2]) / 10
                 PM10 = (data[5] * 256 + data[4]) / 10
-                print("Received valid data PM2,5 {} PM10 {}".format(PM25, PM10))
-                return PM25, PM10
+                print("Received valid data PM2,5 {} PM10 {}".format(PM2_5, PM10))
+                return PM2_5, PM10
 
         raise InvalidDataException
